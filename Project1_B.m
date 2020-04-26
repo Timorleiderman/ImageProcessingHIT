5ed00387255c7b8a9636fa6de72e0c66a2b168f5
Timorleiderman <Timorleiderman@gmail.com>
2020-04-26 12:02:46 +0300

    Timor: added solution for project 1_C

--- Project1_B.m
+++ Project1_B.m
@@ -2,6 +2,7 @@
 clear
 
 I = zeros(313);
+theta = 0:180;
 [m,n] = size(I);
 
 for i = 1:m
@@ -13,17 +14,23 @@ for i = 1:m
     end
 end
 
-R = radon(I);
+[R, xp] = radon(I,theta);
+
 I1 = zeros(313);
 I1(40,40) = 255;
 I1(80,80) = 255;
 I1(124,124) = 255;
 I1(150,150) = 255;
 
-R1 = radon(I1);
-
-
+[R1, xp1] = radon(I1,theta);
 figure(1)
+subplot(2,1,1)
+imagesc(theta,xp,R);
+subplot(2,1,2)
+imagesc(theta,xp1,R1);
+
+
+figure(2)
 subplot(2,2,1)
 imshow(I);
 subplot(2,2,2)
--- Project1_C.m
+++ Project1_C.m
@@ -3,40 +3,102 @@ clear
 
 % find the path to the images
 camera_man_path = which('cameraman.tif');
-camera_man_img = imread(camera_man_path);
+camera_man_img = double(imread(camera_man_path));
 
+% define parameters for angle and length
 L1 = 20;
 L2 = 40;
 alpha = 30;
+theta = 0:180;
 
 [m,n] = size(camera_man_img);
 
+% generate filters
 h1 = fspecial('motion', L1, alpha);
 h2 = fspecial('motion', L2, alpha);
 
-motion_blur_camera_man1 = imfilter(camera_man_img,h1,'replicate');
-motion_blur_camera_man2 = imfilter(camera_man_img,h2,'replicate');
+% apply filters
+motion_blur_camera_man1 = imfilter(camera_man_img,h1,'conv','circular');
+motion_blur_camera_man2 = imfilter(camera_man_img,h2,'conv','circular');
 
+% FFT
 camera_man_img_fft = fftshift(fft2(camera_man_img));
 motion_blur_camera_man_fft1 = fftshift(fft2(motion_blur_camera_man1));
 motion_blur_camera_man_fft2 = fftshift(fft2(motion_blur_camera_man2));
 
+% calc log spectrum
+log_spec_camera_man_fft = abs(log2(camera_man_img_fft));
+log_spec_camera_man_fft1 = abs(log2(motion_blur_camera_man_fft1));
+log_spec_camera_man_fft2 = abs(log2(motion_blur_camera_man_fft2));
+
+% Radon transform
+[R1, xp1] = radon(log_spec_camera_man_fft1, theta);
+[R2, xp2] = radon(log_spec_camera_man_fft2, theta);
+
+% find the peak
+maxR1 = max(R1(:));
+maxR2 = max(R2(:));
+
+[row_idx1, col_idx1] = find(R1 == maxR1);
+[row_idx2, col_idx2] = find(R2 == maxR2);
+
+% find corresponding angle
+angle_blur_camera_man1 = theta(col_idx1);
+angle_blur_camera_man2 = theta(col_idx2);
+
+% apply wiener filter
+wnr_blur_camera_man_1 = deconvwnr(motion_blur_camera_man1,h1);
+wnr_blur_camera_man_2 = deconvwnr(motion_blur_camera_man2,h2);
+
+% plot the resaults
+fig_h = 4;
+fig_w = 3;
+fig_idx = 1;
+
 figure(1)
-subplot(2,3,1)
-imshow(camera_man_img);
+subplot(fig_h,fig_w,fig_idx)
+imshow(uint8(camera_man_img));
 title('Camera man image')
-subplot(2,3,2)
-imshow(motion_blur_camera_man1);
+fig_idx  = fig_idx + 1;
+subplot(fig_h, fig_w, fig_idx)
+imshow(uint8(motion_blur_camera_man1));
 title('motion blur L=20 theta=30')
-subplot(2,3,3)
-imshow(motion_blur_camera_man2);
+fig_idx  = fig_idx + 1;
+subplot(fig_h, fig_w, fig_idx)
+imshow(uint8(motion_blur_camera_man2));
 title('motion blur L=40 theta=30')
-subplot(2,3,4)
-imshow(camera_man_img_fft);
+fig_idx  = fig_idx + 1;
+subplot(fig_h, fig_w, fig_idx)
+imshow(uint8(camera_man_img_fft));
 title('fft of camera man')
-subplot(2,3,5)
-imshow(motion_blur_camera_man_fft1);
+fig_idx  = fig_idx + 1;
+subplot(fig_h, fig_w, fig_idx)
+imshow(uint8(motion_blur_camera_man_fft1));
 title('fft of motion blur L=20')
-subplot(2,3,6)
-imshow(motion_blur_camera_man_fft2);
+fig_idx  = fig_idx + 1;
+subplot(fig_h, fig_w, fig_idx)
+imshow(uint8(motion_blur_camera_man_fft2));
 title('fft of motion blur L=40')
+fig_idx  = fig_idx + 1;
+subplot(fig_h,fig_w,fig_idx)
+imagesc(log_spec_camera_man_fft);
+title('camera man orig')
+axis('off')
+fig_idx  = fig_idx + 1;
+subplot(fig_h, fig_w, fig_idx)
+imagesc(log_spec_camera_man_fft1);
+title('camera man blur L=20')
+axis('off')
+fig_idx  = fig_idx + 1;
+subplot(fig_h,fig_w,fig_idx)
+imagesc(log_spec_camera_man_fft2);
+title('camera man blur L=40')
+axis('off')
+fig_idx  = fig_idx + 2;
+subplot(fig_h,fig_w,fig_idx)
+imshow(uint8(wnr_blur_camera_man_1));
+title('camera man weiner filterr L=20')
+fig_idx  = fig_idx + 1;
+subplot(fig_h,fig_w,fig_idx)
+imshow(uint8(wnr_blur_camera_man_2));
+title('camera man weiner filterr L=40')
