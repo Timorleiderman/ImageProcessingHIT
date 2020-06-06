3c49ed3ec258137024448df0af46a10685d4bd60
Timorleiderman <Timorleiderman@gmail.com>
2020-06-06 15:17:09 +0300

    Timor: added Butterworth  filter to 3B

--- Project3/Project3_B.m
+++ Project3/Project3_B.m
@@ -15,18 +15,23 @@ img_in = imread('Fig1.png');
 
 img_in_double = double(img_in);
 
-sigma = std2(img_in_double)/h;
+sigma = std2(img_in_double)/w;
 
 img_in_log = log(1 + img_in_double);
 
 img_in_fft = fftshift(fft2(img_in_log));
 
-% create gaussian filter
+
 [X, Y] = meshgrid(1:w,1:h);
 centerX = ceil(w/2);
 centerY = ceil(h/2);
 gaussianNumerator = (X-centerX).^2 + (Y-centerY).^2;
-H = 1 - exp(-gaussianNumerator./(2*sigma.^2));
+
+% create gaussian filter
+% H = 1 - exp(-gaussianNumerator./(sigma.^2));
+
+% create Butterworth filter
+H = ( 1./ 1 + (sigma./(X.^2+Y.^2).^0.5).^(2));
 
 % apply gaussian filter
 H_filtered = img_in_fft.*H;
@@ -36,8 +41,8 @@ H_fil_ifft = ifft2(ifftshift(H_filtered));
 G = abs(exp(H_fil_ifft) + 1);
 
 % plot the resaults
-fig_h = 3;
-fig_w = 3;
+fig_h = 2;
+fig_w = 2;
 fig_idx = 1;
 
 figure(1);
@@ -45,16 +50,16 @@ subplot(fig_h,fig_w,fig_idx);
 imshow(uint8(img_in));
 title('Orig image');
 
-fig_idx  = fig_idx + 1;
-subplot(fig_h,fig_w,fig_idx);
-imshow(uint8(abs(img_in_fft)));
-title('img in fft');
-axis('off');
-
-fig_idx  = fig_idx + 1;
-subplot(fig_h,fig_w,fig_idx);
-imshow(uint8(abs(H_filtered)), []);
-title('img filtered');
+% fig_idx  = fig_idx + 1;
+% subplot(fig_h,fig_w,fig_idx);
+% imshow(uint8(abs(img_in_fft)));
+% title('img in fft');
+% axis('off');
+% 
+% fig_idx  = fig_idx + 1;
+% subplot(fig_h,fig_w,fig_idx);
+% imshow(uint8(abs(H_filtered)), []);
+% title('img filtered');
 
 fig_idx  = fig_idx + 1;
 subplot(fig_h,fig_w,fig_idx);
